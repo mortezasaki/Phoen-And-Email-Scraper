aab0f786c50e34b4e46218b0b4452084490ba101
Morteza <mortezasaki91@gmail.com>
2021-01-12 19:11:29 +0330

    First commit

---
+++ run.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+browser = webdriver.Chrome(executable_path='./chromedriver') # Chrome driver
+browser.get('https://divar.ir/my-divar/my-posts')
+
+WebDriverWait(browser,60).until(EC.presence_of_element_located((By.CLASS_NAME, 'login-message__login-btn')))
+
+btn = browser.find_element_by_class_name('login-message__login-btn')
+btn.click()
+input("sdss")
+browser.close()
