0e1a08e629adcadf0913de0bdc23caf5323102c2
Morteza <mortezasaki91@gmail.com>
2021-01-26 19:50:52 +0330

    Fix bugs

--- Divar/run.py
+++ Divar/run.py
@@ -8,6 +8,9 @@ from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import re
 import os
+import requests
+import json
+from time import sleep
 
 chromedriver_path = '.{0}chromedriver'.format(os.sep)
 
@@ -26,14 +29,14 @@ for cookie in cookies_list:
     cookies_dict[cookie['name']] = cookie['value']
 
 token = {'token':cookies_dict['token']}
-f = open('Divar/token.txt','w')
-f.write(cookies_dict['token'])
-f.close()
+# f = open('token.txt','w')
+# f.write(cookies_dict['token'])
+# f.close()
 
 input("For Get links from this page press enter then scroll page...")
 
-if os.path.exists('Divar/contacts.txt'):
-    os.remove('Divar/contacts.txt')
+if os.path.exists('contacts.txt'):
+    os.remove('contacts.txt')
 
 links = []
 
@@ -41,11 +44,24 @@ try:
     while True:
         html_page = browser.page_source
         soup = BeautifulSoup(html_page,'html.parser')
-        with open('Divar/contacts.txt','a') as f:
-            for link in soup.findAll('a', attrs={'href': re.compile("^/v/")}):
-                _id = link.get('href').split('/')[-1]
-                if _id not in links:
-                    links.append(_id)
-                    f.write("%s\n" %(_id))
-except :
+        for link in soup.findAll('a', attrs={'href': re.compile("^/v/")}):
+            _id = link.get('href').split('/')[-1]
+            if _id not in links:
+                url = 'https://api.divar.ir/v5/posts/{0}/contact'.format(_id)
+                req = requests.get(url,cookies=token)
+                if req.status_code == 200:
+                    response =json.loads(req.text)
+                    phone = response['widgets']['contact']['phone']
+                    if re.match('^(09)\d{9}$',phone):
+                        with open('phones.txt','a') as f:
+                            print(phone)
+                            f.write("%s\n" %(phone))
+                    else:
+                        print(phone)
+                links.append(_id)
+            # sleep(1)
+        browser.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
+        # sleep(60*30)
+except Exception as e:
+    print(str(e))
     browser.close()
